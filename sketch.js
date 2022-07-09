b6c063b9655d6ded6a10fff2d5960023fc4d4fb8
yrgaming01 <88611997+yrgaming01@users.noreply.github.com>
2022-07-10 00:21:56 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -14,17 +14,17 @@ var score=0;
 var gameOver, restart;
 
 function preload(){
-  kangaroo_running =   loadAnimation("assets/kangaroo1.png","assets/kangaroo2.png","assets/kangaroo3.png");
-  kangaroo_collided = loadAnimation("assets/kangaroo1.png");
-  jungleImage = loadImage("assets/bg.png");
-  shrub1 = loadImage("assets/shrub1.png");
-  shrub2 = loadImage("assets/shrub2.png");
-  shrub3 = loadImage("assets/shrub3.png");
-  obstacle1 = loadImage("assets/stone.png");
-  gameOverImg = loadImage("assets/gameOver.png");
-  restartImg = loadImage("assets/restart.png");
-  jumpSound = loadSound("assets/jump.wav");
-  collidedSound = loadSound("assets/collided.wav");
+  kangaroo_running =   loadAnimation("kangaroo1.png","kangaroo2.png","kangaroo3.png");
+  kangaroo_collided = loadAnimation("kangaroo1.png");
+  jungleImage = loadImage("bg.png");
+  shrub1 = loadImage("shrub1.png");
+  shrub2 = loadImage("shrub2.png");
+  shrub3 = loadImage("shrub3.png");
+  obstacle1 = loadImage("stone.png");
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+  jumpSound = loadSound("jump.wav");
+  collidedSound = loadSound("collided.wav");
 }
 
 function setup() {
